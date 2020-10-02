dc79398f9bdb60648a474a4663d902649b9361ac
Noah Soublierer <nsoubliere@lcs.on.ca>
2020-10-02 09:04:31 -0400

    made the background and  first squares

--- Playground.playground/Pages/Pixies Poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Pixies Poster.xcplaygroundpage/Contents.swift
@@ -70,6 +70,8 @@ canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height:400)
 
 // draw a line of circles
 let limeGreen = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
+
+
 canvas.fillColor = limeGreen
 for y in stride(from: 0,
                 to: 450,
@@ -111,69 +113,16 @@ canvas.drawText(message: "528 commonwealth", at: Point(x: 230, y: 525), size: 15
 canvas.drawText(message: "boston,bass", at: Point(x: 230, y: 505), size: 15, kerning: 2)
 
 
-//White Circles
 
-canvas.fillColor = Color.white
 
-canvas.drawEllipse(at: Point(x: 50, y: 50), width: 45, height: 45)
 
-canvas.drawEllipse(at: Point(x: 100, y: 50), width: 45, height: 45)
 
-canvas.drawEllipse(at: Point(x: 150, y: 50), width: 45, height: 45)
 
-canvas.drawEllipse(at: Point(x: 200, y: 50), width: 45, height: 45)
 
-canvas.drawEllipse(at: Point(x: 250, y: 50), width: 45, height: 45)
 
-canvas.drawEllipse(at: Point(x: 300, y: 50), width: 45, height: 45)
 
-canvas.drawEllipse(at: Point(x: 350, y: 50), width: 45, height: 45)
 
-canvas.drawEllipse(at: Point(x: 50, y: 100), width: 45, height: 45)
 
-canvas.drawEllipse(at: Point(x: 100, y: 100), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 150, y: 100), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 200, y: 100), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 250, y: 100), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 200, y: 100), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 250, y: 100), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 300, y: 100), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 50, y: 150), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 100, y: 150), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 150, y: 150), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 200, y: 150), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 250, y: 150), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 50, y: 200), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 100, y: 200), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 150, y: 200), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 200, y: 200), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 50, y: 250), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 100, y: 250), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 150, y: 250), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 50, y: 300), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 100, y: 300), width: 45, height: 45)
-
-canvas.drawEllipse(at: Point(x: 50, y: 350), width: 45, height: 45)
 
 //WHITE text
 canvas.textColor = Color.white
