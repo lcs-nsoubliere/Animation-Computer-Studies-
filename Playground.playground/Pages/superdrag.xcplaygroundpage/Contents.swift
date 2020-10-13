bb17a9c8ef3c03f66e28bec8d8bc715241c78068
Noah Soublierer <nsoubliere@lcs.on.ca>
2020-10-13 12:30:29 -0400

    canvas color

---
+++ Playground.playground/Pages/superdrag.xcplaygroundpage/Contents.swift
@@ -0,0 +1,78 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+//: [Previous](@previous) / [Next](@next)
+//: # Blank canvas to copy
+//:
+//: ## To duplicate this page
+//:
+//: Place your cursor on line 19 of this page, press `Command-A` to select all text, and then `Command-C` to copy.
+//:
+//: Two-finger tap on the playground, then choose **New Playground Page**:
+//:
+//: ![new-playground](new-playground.png "New Playground")
+//:
+//: Move to the new page and press `Command-A` to select all text there, then `Command-V` to paste.
+//:
+/*:
+ ## Canvas size
+ 
+ Set the size of your desired canvas by adjusting the constants on lines 19 and 20.
+ */
+let preferredWidth = 400
+let preferredHeight = 600
+/*:
+ ## Required code
+ 
+ Lines 28 to 36 are required to make the playground run.
+ 
+ Please do not remove.
+ */
+import Cocoa
+import PlaygroundSupport
+import CanvasGraphics
+
+// Create canvas
+let canvas = Canvas(width: preferredWidth, height: preferredHeight)
+
+// Show the canvas in the playground's live view
+PlaygroundPage.current.liveView = canvas
+
+//Color
+// COLORS
+let lightBlue = Color(hue: 193, saturation: 100, brightness: 86, alpha: 100)
+let offWhite = Color(hue: 85, saturation: 8, brightness: 88, alpha: 100)
+let translucentPink = Color(hue: 325, saturation: 100, brightness: 81, alpha: 75)
+let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
+
+//canvas backround
+canvas.fillColor = lightBlue
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+//ellipses
+canvas.borderColor = offWhite
+canvas.drawEllipse(at: Point(x: 200, y: 400), width: 5, height: 5, borderWidth: 10)
+
+
+
+// draw the axes with a scale
+canvas.drawAxes(withScale: true, by: 50)
+/*:
+ ## Show the Assistant Editor
+ Don't see any results?
+ 
+ Remember to show the Assistant Editor (1), and then switch to Live View (2):
+ 
+ ![timeline](timeline.png "Timeline")
+
+ ## Use source control
+ To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
+ 
+ Please commit and push your work often.
+ 
+ ![source_control](source-control.png "Source Control")
+ */
