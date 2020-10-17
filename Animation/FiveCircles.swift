bf96ed1b1d4e4c85bd74c5da3702b13c8ca1120a
Noah Soublierer <nsoubliere@lcs.on.ca>
2020-10-17 10:40:41 -0400

    Finished all the circles and there colours

--- Animation/FiveCircles.swift
+++ Animation/FiveCircles.swift
@@ -46,27 +46,36 @@ class FiveCircles: NSObject, Sketchable {
         
         // Change position
         x += 1
+        //Colour
+        canvas.fillColor = Color(hue: 260, saturation: 0, brightness: 0, alpha: 100)
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(at: Point(x: x, y: 50), width: 50, height: 50)
+       
         // Change position
         x1 -= 1
-       //Colour
-        canvas.fillColor = Color(hue: 260, saturation: 100, brightness: 50, alpha: 100)
+        //Colour
+        canvas.fillColor = Color(hue: 260, saturation: 100, brightness: 75, alpha: 100)
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(at: Point(x: x1, y: 150), width: 50, height: 50)
+       
         // Change position
         x2 += 1
         //Colour
+        canvas.fillColor = Color(hue: 140, saturation: 100, brightness: 100, alpha: 100)
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(at: Point(x: x2, y: 250), width: 50, height: 50)
+       
         // Change position
         x3 -= 1
         //Colour
+        canvas.fillColor = Color(hue: 40, saturation: 100, brightness: 100, alpha: 100)
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(at: Point(x: x3, y: 350), width: 50, height: 50)
+        
         // Change position
-        //Colour
         x4 += 1
+        //Colour
+        canvas.fillColor = Color(hue: 280, saturation: 100, brightness: 80, alpha: 100)
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(at: Point(x: x4, y: 450), width: 50, height: 50)
     }
