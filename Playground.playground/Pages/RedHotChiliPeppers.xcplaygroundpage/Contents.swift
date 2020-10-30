6264553ec5a917161443f49122794a3f0bb78f82
Noah Soublierer <nsoubliere@lcs.on.ca>
2020-10-30 08:45:05 -0400

    Got the canvas info

--- Playground.playground/Pages/RedHotChiliPeppers.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/RedHotChiliPeppers.xcplaygroundpage/Contents.swift
@@ -1,3 +1,4 @@
+//: [Previous](@previous)
 
 import Foundation
 
@@ -49,40 +50,16 @@ PlaygroundPage.current.liveView = canvas
  
  */
 //Colours
-let orange = Color(hue: 21, saturation: 76, brightness: 94, alpha: 100)
-let blue = Color(hue: 195, saturation: 100, brightness: 30, alpha: 80)
 let offWhite = Color(hue: 208, saturation: 1, brightness: 88, alpha: 100)
-let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 20)
+let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
 let deepRed = Color(hue: 5, saturation: 85, brightness: 94, alpha: 100)
 
 //Fill Canvas
-canvas.fillColor = deepRed
+canvas.fillColor = B
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
 canvas.drawShapesWithBorders = false
 
-//draw squares
-for y in stride(from: 0, to: 400, by: 100) {
-    for x in stride(from: 0, to: 400, by: 100){
- 
-        canvas.fillColor = offWhite
-        var vertices: [Point] = [] //empty list of vertices
-        vertices.append(Point(x: x + 50, y: y))
-        vertices.append(Point(x: x + 100, y: y + 50))
-        vertices.append(Point(x: x + 50, y: y + 100))
-        vertices.append(Point(x: x, y: y + 50))
-        canvas.drawCustomShape(with: vertices)
-      
-        canvas.fillColor = deepRed
-        canvas.drawEllipse(at: Point(x: x + 50, y: y + 50), width: 65, height: 65)
-   
-        canvas.fillColor = blue
-        canvas.drawEllipse(at: Point(x: x + 75, y: y + 25), width: 65, height: 65)
-        
-        canvas.fillColor = black
-        canvas.drawEllipse(at: Point(x: x + 25, y: y + 75), width: 65, height: 65)
-    }
-}
 
 // draw the axes with a scale
 canvas.drawAxes(withScale: true, by: 50)
@@ -101,5 +78,7 @@ canvas.drawAxes(withScale: true, by: 50)
  
  ![source_control](source-control.png "Source Control")
  */
+
+//: [Next](@next)
 
 //: [Next](@next)
