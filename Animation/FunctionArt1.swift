8c0bf8f62addc95d130d5dc2dbb3593ec7444577
Noah Soublierer <nsoubliere@lcs.on.ca>
2020-11-19 14:25:16 -0500

    changed the animation so that the entirety of the animation is draw within a single frame

--- Animation/FunctionArt1.swift
+++ Animation/FunctionArt1.swift
@@ -10,27 +10,27 @@ import CanvasGraphics
 
 // NOTE: This is a completely empty sketch; it can be used as a template.
 class FunctionArt1: NSObject, Sketchable {
-
+    
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     var canvas: Canvas
-
+    
     // Add many functions
     // This is now a list, or an array, of functions
     var functions: [MathFunction] = []    // empty list
     
     // This function runs once
     override init() {
-     
+        
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
-             
+        
         canvas.lineColor = Color.init(hue: Int.random(in: 1...360),
                                       saturation:  Int.random(in: 1...100),
                                       brightness:  Int.random(in: 1...100),
                                       alpha:  Int.random(in: 25...100))
         
-
+        
         // Initialize many functions
         for i in 1...20 {
             
@@ -44,17 +44,17 @@ class FunctionArt1: NSObject, Sketchable {
             
             // Add it to the list
             functions.append(newFunction)
-           
+            
             
         }
         
         // Speed
         canvas.framesPerSecond = 60
     }
-
+    
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
-
+        
         //clear the canvas
         canvas.fillColor = Color(hue: 0,
                                  saturation: 0,
@@ -64,20 +64,34 @@ class FunctionArt1: NSObject, Sketchable {
         
         
         // What frame are we on?
-//        print(canvas.frameCount)
+        //        print(canvas.frameCount)
         
         canvas.defaultLineWidth = 1
-      
+        
         // Set the origin to be the middle of the canvas
         canvas.translate(to: Point(x: canvas.width / 2, y: canvas.height / 2))
-
-        // Update the position of all functions
-        for function in functions {
-            function.update(on: canvas,
-                            usingInputValue: canvas.frameCount)
+        
+        //draw the entire list of functions all at once
+        for x in 0...canvas.width {
+   
+            // Update the position of all functions
+            for function in functions {
+                function.update(on: canvas,
+                                usingInputValue: x)
+            }
+            
+            
+            
         }
-
-    
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
-
+    
 }
